987fb46c261e41ed5a0940cc0d0eaa0bda9d3ac6
Jihan Lee <jihan.lee@deneer.co.kr>
2022-04-28 09:47:15 +0900

    Add prune script for local

---
+++ prune-local-branches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# CAUTION: remote branch 에 존재하지 않는 local branch 를 삭제합니다.
+# 백업이 필요하다면 git archive 를 활용하세요.
+
+#!/bin/bash
+
+# CAUTION: remote branch 에 존재하지 않는 local branch 를 삭제합니다.
+# 백업이 필요하다면 git archive 를 활용하세요.
+
+for target in $(git branch | tee | grep -v "*")
+do
+    [[ $(git branch -r | grep $target | wc -l) -gt 0 ]] \
+    && echo "Remote branch detected :"$target \
+    || (git branch -D $target && echo "["$target"] branch is deleted")
+done;
